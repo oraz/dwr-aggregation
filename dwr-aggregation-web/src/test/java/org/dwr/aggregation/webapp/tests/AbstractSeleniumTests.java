6767e60e4620547ad050f7fc495b1de8347d3c46
oraz <oraz.ra@gmail.com>
2013-11-14 19:08:41 +0400

    Selenium tests

--- dwr-aggregation-web/src/test/java/org/dwr/aggregation/webapp/tests/AbstractSeleniumTests.java
+++ dwr-aggregation-web/src/test/java/org/dwr/aggregation/webapp/tests/AbstractSeleniumTests.java
@@ -6,12 +6,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterGroups;
 import org.testng.annotations.BeforeGroups;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static java.lang.Integer.parseInt;
+
 public abstract class AbstractSeleniumTests {
     protected final String Acceptance = "acceptance";
     private WebDriver driver;
+    private Properties acceptanceProperties;
 
     @BeforeGroups(groups = Acceptance)
-    public final void initDriver() {
+    public final void initDriver() throws IOException {
+        loadAcceptanceProperties();
         driver = new FirefoxDriver();
     }
 
@@ -20,18 +28,30 @@ public abstract class AbstractSeleniumTests {
         driver.quit();
     }
 
+    private void loadAcceptanceProperties() throws IOException {
+        final InputStream resource = getClass().getClassLoader().getResourceAsStream("acceptance.properties");
+        acceptanceProperties = new Properties();
+        acceptanceProperties.load(resource);
+    }
+
     protected <T extends AbstractPage> T getPage(final String pageUrl, final Class<T> pageClass) {
-        final T page;
+        final T page = newInstance(pageClass);
+        page.init(driver);
+        driver.get(String.format("http://%s:%d%s%s",
+                acceptanceProperties.getProperty("application.http.host"),
+                parseInt(acceptanceProperties.getProperty("application.http.port")),
+                acceptanceProperties.getProperty("contextPath"),
+                pageUrl));
+        return page;
+    }
+
+    private <T> T newInstance(final Class<T> pageClass) {
         try {
-            page = pageClass.newInstance();
+            return pageClass.newInstance();
         } catch (InstantiationException e) {
             throw new AssertionError(e);
         } catch (IllegalAccessException e) {
             throw new AssertionError(e);
         }
-
-        page.init(driver);
-        driver.get("http://localhost:8080/aggr" + pageUrl);
-        return page;
     }
 }
