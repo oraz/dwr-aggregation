3a7dd3d6a26b8b43858dc269edf7449cdfd7db23
oraz <oraz.ra@gmail.com>
2013-11-15 13:05:33 +0400

    Removed jQuery

--- dwr-aggregation-web/src/main/webapp/html/common.js
+++ dwr-aggregation-web/src/main/webapp/html/common.js
@@ -1,21 +1,31 @@
 'use strict';
 
-jQuery(document).ready(function ($) {
+function onLoad(handler) {
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', handler, false);
+    } else {
+        window.attachEvent('onload', handler);
+    }
+}
 
+onLoad(function () {
     usersManager.getAllUsers(function (users) {
-        var list$ = $('<ul/>', { id: 'users'});
-        $.each(users, function () {
-            $('<li/>', { text: this }).appendTo(list$);
-        });
-        list$.appendTo(document.body);
+        createList(users, 'users');
     });
 
     citiesManager.getAllCities(function (cities) {
-        var list$ = $('<ul/>', { id: 'cities'});
-        $.each(cities, function () {
-            $('<li/>', { text: this }).appendTo(list$);
-        });
-        list$.appendTo(document.body);
+        createList(cities, 'cities');
     });
 
+    function createList(data, elemId) {
+        var list = document.createElement('ul');
+        list.setAttribute('id', elemId);
+        for (var i = 0; i < data.length; i++) {
+            var item = document.createElement('li'),
+                text = document.createTextNode(data[i]);
+            item.appendChild(text);
+            list.appendChild(item);
+        }
+        document.body.appendChild(list);
+    }
 });
